78fb6a14ab4cadb85f3b8d65fdb94c9bfc27562f
Mihail1604 <113554318+Mihail1604@users.noreply.github.com>
2022-12-08 01:18:47 +0300

    Update calc_itog.cs

--- calc_itog.cs
+++ calc_itog.cs
@@ -6,21 +6,18 @@ namespace calc
 {
     class Program
     {
-        //Метод возвращает true, если проверяемый символ - оператор
         static private bool IsOperator(char с)
         {
             if ("+-/*()#".IndexOf(с) != -1)
                 return true;
             return false;
         }
-        //Метод возвращает true, если проверяемый символ - разделитель ("пробел")
         static private bool IsDelimeter(char c)
         {
             if (c == ' ' || c=='\t')
                 return true;
             return false;
         }
-        //Метод возвращает приоритет оператора
         static private byte GetPriority(char s)
         {
             switch (s)
@@ -34,7 +31,6 @@ namespace calc
                 default: return 4;
             }
         }
-        //Метод, преобразующий входную строку с выражением в постфиксную запись
         static private string GetExpression(string input)
         {
             input = input.Replace(".", ",");
@@ -42,41 +38,36 @@ namespace calc
                 input = Regex.Replace(input, @"pow\((?<first>.*?),(?<second>.*?)\)", "((${first})#(${second}))");
             if (input.IndexOf(",,") != -1)
                 return "Некоректный формат чисел!";
-            string output = string.Empty; //Строка для хранения выражения
-            Stack<char> operStack = new Stack<char>(); //Стек для хранения операторов
-            for (int i = 0; i < input.Length; i++) //Для каждого символа в входной строке
+            string output = string.Empty; 
+            Stack<char> operStack = new Stack<char>(); 
+            for (int i = 0; i < input.Length; i++) 
             {
-                //Разделители пропускаем
                 if (IsDelimeter(input[i]))
-                    continue; //Переходим к следующему символу
-                //Если символ - цифра, то считываем все число
-                else if (char.IsDigit(input[i]) || input[i] == ',') //Если цифра 
+                    continue; 
+                else if (char.IsDigit(input[i]) || input[i] == ',')  
                 {
                     string tempOut = string.Empty;
-                    //Читаем до разделителя или оператора, что бы получить число
                     while ((!IsDelimeter(input[i])) && !IsOperator(input[i]))
                     {
                         if (!char.IsDigit(input[i]) && input[i] != ',')
                         return "Ошибка! " + i + 1 + "-ый символ не является символом операции или цифрой";
-                        tempOut += input[i]; //Добавляем каждую цифру числа к нашей строке
-                        i++; //Переходим к следующему символу
-                        if (i == input.Length) break; //Если символ - последний, то выходим из цикла
+                        tempOut += input[i]; 
+                        i++; 
+                        if (i == input.Length) break; 
                     }
-                    output += tempOut + " "; //Дописываем после числа пробел в строку с выражением
-                    i--; //Возвращаемся на один символ назад, к символу перед разделителем
+                    output += tempOut + " "; 
+                    i--; 
                 }
-                //Если символ - оператор
-                else if (IsOperator(input[i])) //Если оператор
+                else if (IsOperator(input[i])) 
                 {
                     if (i + 1 < input.Length)
-                        if (input[i] == '(' && input[i + 1] == '-') //Если символ - открывающая скобка и "-" после нее
-                            output += "0 "; //Записываем 0
+                        if (input[i] == '(' && input[i + 1] == '-') 
+                            output += "0 "; 
  
-                    if (input[i] == '(') //Если символ - открывающая скобка
-                        operStack.Push(input[i]); //Записываем её в стек
-                    else if (input[i] == ')') //Если символ - закрывающая скобка
+                    if (input[i] == '(') 
+                        operStack.Push(input[i]); 
+                    else if (input[i] == ')') 
                     {
-                        //Выписываем все операторы до открывающей скобки в строку
                         try
                         {
                             char s = operStack.Pop();
@@ -91,47 +82,45 @@ namespace calc
                             return "Ошибка! Неправильно расставлены скобки!";
                         }
                     }
-                    else //Если любой другой оператор
+                    else 
                     {
-                        if (operStack.Count > 0) //Если в стеке есть элементы
-                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek())) //И если приоритет нашего оператора меньше или равен приоритету оператора на вершине стека
-                                output += operStack.Pop().ToString() + " "; //То добавляем последний оператор из стека в строку с выражением
-                        operStack.Push(char.Parse(input[i].ToString())); //Если стек пуст, или же приоритет оператора выше - добавляем операторов на вершину стека
+                        if (operStack.Count > 0) 
+                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek())) 
+                                output += operStack.Pop().ToString() + " "; 
+                        operStack.Push(char.Parse(input[i].ToString())); 
                     }
                 }
                 else return "Ошибка! " + (i + 1) + "-ый символ не является символом операции или цифрой";
             }
-            //Когда прошли по всем символам, выкидываем из стека все оставшиеся там операторы в строку 
+            
             while (operStack.Count > 0)
                 output += operStack.Pop() + " ";
-            return output; //Возвращаем выражение в постфиксной записи
+            return output; 
         }
-        //Метод, вычисляющий значение выражения, уже преобразованного в постфиксную запись
         static private double Counting(string input)
         {
-            double result = double.NaN; //Результат
-            Stack<double> temp = new Stack<double>(); //Временный стек для решения 
+            double result = double.NaN; 
+            Stack<double> temp = new Stack<double>();  
  
-            for (int i = 0; i < input.Length; i++) //Для каждого символа в строке
+            for (int i = 0; i < input.Length; i++) 
             {
-                //Если символ - цифра, то читаем все число и записываем на вершину стека
                 if (char.IsDigit(input[i])||input[i]==',')
                 {
                     string a = string.Empty;
  
-                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) //Пока не разделитель
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
                     {
-                        a += input[i]; //Добавляем
+                        a += input[i];
                         i++;
                         if (i == input.Length) break;
                     }
                     a = a.Replace(",", "0,");
-                    temp.Push(Convert.ToDouble(a)); //Записываем в стек
+                    temp.Push(Convert.ToDouble(a)); 
                     i--;
                 }
-                else if (IsOperator(input[i])) //Если символ - оператор
+                else if (IsOperator(input[i])) 
                 {
-                    //Берем два последних значения из стека
+
                     try
                     {
                         double a = temp.Pop();
@@ -141,7 +130,7 @@ namespace calc
                         else
                             b = temp.Pop();
  
-                        switch (input[i]) //И производим над ними действие, согласно оператору
+                        switch (input[i]) 
                         {
                             case '+':
                                 result = b + a; break;
@@ -164,7 +153,7 @@ namespace calc
                                 }
                                 result = b / a; break;
                         }
-                        temp.Push(result); //Результат вычисления записываем обратно в стек
+                        temp.Push(result); 
                     }
                     catch (InvalidOperationException)
                     {
@@ -175,7 +164,7 @@ namespace calc
             }
             try
             {
-                return temp.Peek(); //Забираем результат всех вычислений из стека и возвращаем его
+                return temp.Peek();
             }
             catch (InvalidOperationException)
             {
@@ -183,22 +172,21 @@ namespace calc
                 return double.NaN;
             }
         }
-        //Метод Calculate принимает выражение в виде строки и возвращает результат, в своей работе использует другие методы класса
         static public double Calculate(string input)
         {
-            string output = GetExpression(input); //Преобразовываем выражение в постфиксную запись
+            string output = GetExpression(input); 
             Console.WriteLine("Преобразованное выражение: " + output.Replace("#", "pow"));
-            double result = Counting(output); //Решаем полученное выражение
-            return result; //Возвращаем результат
+            double result = Counting(output); 
+            return result; 
         }
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Write("Введите выражение: "); //Предлагаем ввести выражение
+                Console.Write("Введите выражение: "); 
                 string parsestr = Console.ReadLine();
                 if (parsestr == "") break;
-                Console.WriteLine("Результат: " + Calculate(parsestr)); //Считываем, и выводим результат
+                Console.WriteLine("Результат: " + Calculate(parsestr)); 
             }
         }
     }
